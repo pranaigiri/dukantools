5e06dbb804ccf0b287334bc5308f86a7e025a5c2
Pranai Giri <pranaigirisikkim@gmail.com>
2023-08-07 13:55:42 +0530

    Added Padding in Coming soon widget

--- lib/screens/coming_soon.dart
+++ lib/screens/coming_soon.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:gram_or_price/common/banner_ad.dart';
 
 class ComingSoon extends StatelessWidget {
   const ComingSoon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "More tools to be added soon...",
-        textAlign: TextAlign.center,
+    return const Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Center(
+        child: Text(
+          "More tools to be added soon...",
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
